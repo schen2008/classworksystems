ed2c8bb881dc778813e00a3a73e6911222d564fc
Sean Chen <schen200826@gmail.com>
2025-11-01 01:24:33 -0400

    wrote horizontal, test later

--- 2025-10-29-Triangles/main.c
+++ 2025-10-29-Triangles/main.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+int err(){
+    printf("errno %d\n",errno);
+    printf("%s\n",strerror(errno));
+    exit(1);
+}
+int isTriangle(int a, int b, int c){
+  return (a+b>c) && (a+c>b) && (b+c>a);
+}
 int main(int argc, char* argv[]){
-  FILE * input = fopen("data", r);
-  int x,y,z;
-  if(argc > 1){
-    if(strcmp(argv[1], "user") == 0){
-      char * buffer = malloc(sizeof(int)*10);
-      buffer = fgets(buffer,10,input);
-      sscanf(buffer, "%d %d %d", &x, &y, &z);
+  return 0;
+}
+int horizontal(char *fileName){
+  FILE *fd = fopen(fileName,"r");
+  if(fd==NULL){
+    err();
+    return -1;
+  }
+  int a,b,c;
+  int valid = 0;
+  char buffer[64]; // no idea if this is big enough
+  while(fgets(buffer, sizeof(buffer), fd) != NULL){
+    if(sscanf(fd, "%d %d %d", &a, &b, &c) == 3){
+      if(isTriangle(a,b,c)) valid++;
     }
   }
+  fclose(fd);
+  printf("%d Valid Triangles\n", valid);
+  return valid;
 }
