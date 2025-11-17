04e30acbf81be1b35387f299eaf7d3934e87c900
Sean Chen <schen200826@gmail.com>
2025-11-16 19:41:55 -0500

    tested cmds

--- 2025-11-14-Exec/main.c
+++ 2025-11-14-Exec/main.c
@@ -4,6 +4,8 @@
 #include <unistd.h>
 
 int main(int argc, char* argv[]){
-  execvp("/bin", argv);
+  // execlp("ls","ls","-a", "-l",NULL);
+  // execvp(argv[1],&argv[1]);
+  execl("/bin/ls","ls","-a","-l",NULL);
   return 0;
 }
